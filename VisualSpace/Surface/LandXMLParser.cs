f99ba41b33f78be7e756aa2171ee5e31f21e7b87
Reese Walton <Reese.Walton@hdrinc.com>
2024-12-13 11:48:46 -0500

    Change LandXML face calculations

--- VisualSpace/Surface/LandXMLParser.cs
+++ VisualSpace/Surface/LandXMLParser.cs
@@ -2,29 +2,19 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
 using System.Xml;
-using HelixToolkit.Wpf;
 
 namespace VisualSpace;
 
 static class LandXMLParser
 {
-    private class LandXMLPointCollection : KeyedCollection<int, LandXMLPoint>
-    {
-        protected override int GetKeyForItem(LandXMLPoint item) => item.Id;
-    }
 
-    private record struct LandXMLPoint(int Id, Point3D Coordinates);
-
-    public static MeshGeometry3D ParseSurface(string xmlPath)
+    public static MeshGeometry3D ParseSurface(System.IO.Stream xmlStream)
     {
-        LandXMLPointCollection pointList = new();
 
         XmlDocument xmlDoc = new();
-        xmlDoc.Load(xmlPath);
+        xmlDoc.Load(xmlStream);
         XmlNode landXml = xmlDoc.DocumentElement;
 
         XmlNamespaceManager ns = new(xmlDoc.NameTable);
@@ -32,36 +22,43 @@ static class LandXMLParser
 
         if (landXml.SelectSingleNode("//l:Surface", ns) is XmlNode surfDef)
         {
-            MeshBuilder builder = new();
+            MeshGeometry3D mesh = new();
+            Dictionary<uint, int> pointIdMap = new();
+
             XmlNode points = surfDef.SelectSingleNode("//l:Pnts", ns);
             foreach (XmlNode p in points.SelectNodes("l:P", ns))
             {
-                var pointId = int.Parse(p.Attributes["id"]?.Value);
+                uint pointId = uint.Parse(p.Attributes["id"]?.Value);
                 var pointCoords = p.InnerText.Split(' ').Select(double.Parse).ToList();
                 if (pointCoords.Count != 3)
                 {
                     throw new Exception($"Invalid LandXML - Point has {pointCoords.Count} coordinates");
                 }
+
                 // LandXML coordinate are represented as "northing easting elevation"
-                pointList.Add(new LandXMLPoint(pointId, new Point3D(pointCoords[1], pointCoords[0], pointCoords[2])));
+                var point = new Point3D(pointCoords[1], pointCoords[0], pointCoords[2]);
+                pointIdMap.Add(pointId, pointIdMap.Count);
+                mesh.Positions.Add(point);
             }
 
             XmlNode faces = surfDef.SelectSingleNode("//l:Faces", ns);
             foreach (XmlNode f in faces.SelectNodes("l:F", ns))
             {
-                List<int> pointIds = f.InnerText.Split(' ').Select(int.Parse).ToList();
+                List<uint> pointIds = f.InnerText.Split(' ').Select(uint.Parse).ToList();
                 if (pointIds.Count != 3)
                 {
                     throw new Exception($"Invalid LandXML - Face has {pointIds.Count} points");
                 }
-
-                builder.AddTriangle(pointList[pointIds[0]].Coordinates, pointList[pointIds[1]].Coordinates, pointList[pointIds[2]].Coordinates);
+                // let WPF calculate vertex normals as average of connected planes (will result in smooth edges instead of visible lines)
+                mesh.TriangleIndices.Add(pointIdMap[pointIds[0]]);
+                mesh.TriangleIndices.Add(pointIdMap[pointIds[1]]);
+                mesh.TriangleIndices.Add(pointIdMap[pointIds[2]]);
             }
-            return builder.ToMesh(true);
+            return mesh;
         }
         else
         {
-            throw new Exception($"No surface definition in file {xmlPath}");
+            throw new Exception($"No surface definition found");
         }
     }
 }
